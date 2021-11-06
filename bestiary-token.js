74f2680d8ee083c8199420b24d9b526c55456309
Doctor-Unspeakable <71834124+Doctor-Unspeakable@users.noreply.github.com>
2021-11-06 21:27:32 +0000

    Update bestiary-token.js

--- bestiary-token.js
+++ bestiary-token.js
@@ -1,7 +1,7 @@
 Hooks.on('preCreateActor', function(obj, html){
     var fileName = obj.data.name.split(" (")[0].trim();
-    console.log("Bestiary Token creating "+fileName);
-    var nameAddress = "https://assets.forge-vtt.com/61648b1349625b2b658890e3/tokens/"+encodeURIComponent(fileName)+".png";
-    obj.data.token._source.img=nameAddress;
+    console.log("Bestiary Token creating "+fileName); 
+    var nameAddress = "https://assets.forge-vtt.com/61648b1349625b2b658890e3/tokens/"+fileName+".png";
+    obj.data.token._source.img=encodeURI(nameAddress);
     obj.data._source.img=nameAddress;
 });
